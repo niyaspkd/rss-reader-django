1c46df5376e3aa6687139baabc9e5893cb9fe74e
niyas <niyasahamedpkd@gmil.com>
2017-03-01 16:17:47 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # django-rss-reader
+# rss-reader-django
--- rss_feed/settings.py
+++ rss_feed/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'feed_reader',
 )
 
 MIDDLEWARE_CLASSES = (
